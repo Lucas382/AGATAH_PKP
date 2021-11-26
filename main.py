2e9e6a9db79526c407b0a7e0f4819d936f9e0405
Lucas Alves <lucasalves382@hotmail.com>
2021-11-26 14:53:17 -0300

    Deleted previous functionality and added a new, that's creates new Layouts dynamically using Kwargs based on a list

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
 
 
-class Incrementador(BoxLayout):
-    pass
+class Tarefas(BoxLayout):
+    #Define o metodo de inicialização da classe
+    def __init__(self,tarefas, **kwargs):
+        super().__init__(**kwargs)  #--> chama o metodo de inicialização do Super(BoxLayout)
+        for tarefa in tarefas:
+            self.add_widget(Label(text=tarefa, font_size=30))
+
 
 class Test(App):
     def build(self):
-        return Incrementador()
+        return Tarefas(['Fazer compras', 'Buscar filho'], orientation='horizontal')
 
 
 if __name__ == '__main__':
