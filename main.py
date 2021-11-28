885b750f9a5bc005b50ae1edbc080fdfd739bf20
Lucas Alves <lucasalves382@hotmail.com>
2021-11-28 15:05:26 -0300

    Add a Scroll view to see the list of tasks.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
 
 
-class Tarefas(BoxLayout):
+class Tarefas(ScrollView):
     #Define o metodo de inicialização da classe
     def __init__(self,tarefas, **kwargs):
         super().__init__(**kwargs)  #--> chama o metodo de inicialização do Super(BoxLayout)
         for tarefa in tarefas:
-            self.add_widget(Label(text=tarefa, font_size=30))
+            self.ids.box.add_widget(Label(text=tarefa, font_size=30, size_hint_y =None, height=200))
 
 
 class Test(App):
     def build(self):
-        return Tarefas(['Fazer compras', 'Buscar filho'], orientation='horizontal')
+        return Tarefas(['Fazer compras', 'Buscar filho', 'Buscar filho', 'Buscar filho', 'Buscar filho', 'Buscar filho'])
 
 
 if __name__ == '__main__':
