ec952b9cce52436916f53582379006b9fe02fb81
Lucas Alves <lucasalves382@hotmail.com>
2021-11-28 15:35:42 -0300

    Add a TextInput and Button to ADD a new task("Tarefa") and a Button to REMOVE a task("Tarefa").

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.scrollview import ScrollView
 
 
-class Tarefas(ScrollView):
+
+class Tarefas(BoxLayout):
     #Define o metodo de inicialização da classe
     def __init__(self,tarefas, **kwargs):
         super().__init__(**kwargs)  #--> chama o metodo de inicialização do Super(BoxLayout)
         for tarefa in tarefas:
-            self.ids.box.add_widget(Label(text=tarefa, font_size=30, size_hint_y =None, height=200))
+            self.ids.box.add_widget(Tarefa(text=tarefa))
+
+
+    def add_widget_tarefas(self):
+        texto = self.ids.texto.text
+        self.ids.box.add_widget(Tarefa(text=texto))
+        self.ids.texto.text = ''
+
+
+class Tarefa(BoxLayout):
+    def __init__(self,text='', **kwargs):
+        super().__init__(**kwargs)
+        self.ids.label.text = text
 
 
 class Test(App):
     def build(self):
-        return Tarefas(['Fazer compras', 'Buscar filho', 'Buscar filho', 'Buscar filho', 'Buscar filho', 'Buscar filho'])
+        return Tarefas(['Fazer compras', 'Buscar filho'])
 
 
 if __name__ == '__main__':
