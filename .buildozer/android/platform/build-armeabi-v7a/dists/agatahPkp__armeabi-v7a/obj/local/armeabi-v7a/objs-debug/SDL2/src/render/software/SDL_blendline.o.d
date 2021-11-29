6fd22b9ca2bf2f6c2608bf3825826d3cd131370e
Lucas Alves <lucasalves382@hotmail.com>
2021-11-29 10:54:38 -0300

    Generated the first APK for debugging tests.

---
+++ .buildozer/android/platform/build-armeabi-v7a/dists/agatahPkp__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/SDL2/src/render/software/SDL_blendline.o.d
@@ -0,0 +1,68 @@
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2/src/render/software/SDL_blendline.o: \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/SDL_blendline.c \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../SDL_internal.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../dynapi/SDL_dynapi.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../dynapi/SDL_dynapi_overrides.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/SDL_draw.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../video/SDL_blit.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../video/../SDL_internal.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_cpuinfo.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_endian.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_surface.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_pixels.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rect.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_error.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rwops.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_blendmode.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/SDL_blendline.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/SDL_blendpoint.h
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../SDL_internal.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../dynapi/SDL_dynapi.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../dynapi/SDL_dynapi_overrides.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/SDL_draw.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../video/SDL_blit.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/../../video/../SDL_internal.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_cpuinfo.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_endian.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_surface.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_pixels.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rect.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_error.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rwops.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_blendmode.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/SDL_blendline.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/render/software/SDL_blendpoint.h:
---
+++ .buildozer/android/platform/build-armeabi-v7a/dists/agatahPkp__armeabi-v7a/obj/local/armeabi-v7a/objs-debug/SDL2/src/test/SDL_test_imageBlitBlend.o.d
@@ -0,0 +1,179 @@
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/obj/local/armeabi-v7a/objs-debug/SDL2/src/test/SDL_test_imageBlitBlend.o: \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/src/test/SDL_test_imageBlitBlend.c \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_main.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_assert.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_atomic.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_audio.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_error.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_endian.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mutex.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_thread.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rwops.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_clipboard.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_cpuinfo.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_events.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_video.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_pixels.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rect.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_surface.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_blendmode.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keyboard.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keycode.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_scancode.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mouse.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_joystick.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gamecontroller.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_quit.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gesture.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_touch.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_filesystem.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_haptic.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_hints.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_loadso.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_log.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_messagebox.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_power.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_render.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_sensor.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_shape.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_system.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_timer.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_version.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_assert.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_common.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_compare.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_images.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_crc32.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_font.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_fuzzer.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_harness.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_log.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_md5.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_memory.h \
+  /mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_random.h
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_platform.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/begin_code.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/close_code.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_config_android.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_main.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_stdinc.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_assert.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_atomic.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_audio.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_error.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_endian.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mutex.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_thread.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rwops.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_clipboard.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_cpuinfo.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_events.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_video.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_pixels.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_rect.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_surface.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_blendmode.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keyboard.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_keycode.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_scancode.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_mouse.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_joystick.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gamecontroller.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_quit.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_gesture.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_touch.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_filesystem.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_haptic.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_hints.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_loadso.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_log.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_messagebox.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_power.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_render.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_sensor.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_shape.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_system.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_timer.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_version.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_assert.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_common.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_compare.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_images.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_crc32.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_font.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_fuzzer.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_harness.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_log.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_md5.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_memory.h:
+
+/mnt/c/users/lucas/AGATAH_PKP/.buildozer/android/platform/build-armeabi-v7a/build/bootstrap_builds/sdl2/jni/SDL/include/SDL_test_random.h:
